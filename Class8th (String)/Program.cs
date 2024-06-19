901f9390c46769825effecb9cc5932ec77173dab
BaeGWoo <gwbae4211@naver.com>
2024-06-19 15:41:49 +0900

    (2024.06.19) 문자열

---
+++ Class8th (String)/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace Class8th__String_
+{
+    public class String
+    {
+        private char[] arr;
+        int size;
+
+        public String()
+        {
+            arr = null;
+            size = 0;
+        }
+
+
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Add(char[] content)
+        {
+            int newSize = content.Length;
+            arr = new char[newSize + 1];
+            for (int i = 0; i < newSize; i++)
+            {
+                arr[i] = content[i];
+            }
+            size = newSize;
+        }
+
+
+        public void Show()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+        public void ConCat(char[] content)
+        {
+            int newSize = content.Length + size;
+            char[] newArr = new char[newSize + 1];
+            for (int i = 0; i < size; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                newArr[i + size] = content[i];
+            }
+            arr = newArr;
+            size = newSize;
+        }
+
+        public bool Equal(char[] content)
+        {
+            if (size != content.Length)
+                return false;
+
+            for (int i = 0; i < size; i++)
+            {
+                //int pivot = arr[i];
+                //int target = (int)content[i];
+                if ((int)arr[i] != (int)content[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public int IndexOf(char target)
+        {
+            int result = -1;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == target)
+                {
+                    result = i;
+                    //return result;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public bool Contains(char[] target)
+        {
+            int count = 0;
+            for (int i = 0; i < size - target.Length + 1; i++)
+            {
+                for (int j = 0; j < target.Length; j++)
+                {
+                    if (arr[i + j] == target[j])
+                        count++;
+
+                    if (count == target.Length)
+                        return true;
+                }
+                count = 0;
+            }
+            return false;
+        }
+
+
+        public bool Contain2(char[] target)
+        {
+            int index = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == target[index])
+                    index++;
+                else index = 0;
+            }
+
+            if (index == target.Length)
+                return true;
+            else return false;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // String str1 = new String();
+           // str1.Add(new char[] { 'A', '2', '3', 'B', 'C' });
+           // //str1.Add(new char[] {  });
+           // str1.Show();
+           // Console.WriteLine();
+           // //str1.ConCat(new char[] { '9', '7' });
+           // Console.WriteLine("str1의 크기 : " + str1.Size());
+           // str1.Show();
+           //
+           // Console.WriteLine();
+           // Console.WriteLine(str1.Equal(new char[] { 'A', '2', '3', 'C', 'D' }));
+           // //Console.WriteLine(str1.Equal(new char[] {  }));
+           // Console.WriteLine(str1.IndexOf('2'));
+           //
+           // Console.WriteLine(str1.Contains(new char[] { '3', 'B', 'C' }));
+           // Console.WriteLine(str1.Contain2(new char[] { '3', 'B', 'C' }));
+
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -3,109 +3,13 @@ using System.Drawing;
 
 namespace Program
 {
-  
-public class Vector<T>
-    {
-        private int size;
-        private int capacity;
-        private T[] arr;
-        private T error;
-
-        public Vector()
-        {
-            size = 0;
-            capacity = 0;
-            arr = null;
-        }
-
-        public int Size()
-        {
-            return size;
-        }
-
-        public void Resize(int newSize)
-        {
-            T[] temp = new T[newSize];
-            for (int i = 0; i < size; i++)
-            {
-                temp[i] = arr[i];
-            }
-            arr = temp;
-            temp = null;
-            capacity = newSize;
-        }
-
-        public void Add(T data)
-        {
-            if (arr == null)
-            {
-                Resize(1);
-            }
-
-            else
-            {
-                if (size == capacity)
-                {
-                    Resize(capacity * 2);
-                }
-            }
-            arr[size] = data;
-            size++;
-        }
-
-        public void Reserve(int newSize)
-        {
-            if (newSize > capacity)
-            {
-                Resize(newSize);
-            }
-        }
-
-        public void RemoveAt(int index)
-        {
-            if (index < size)
-            {
-                for(int i = index; i < size-1; i++)
-                {
-                    arr[i] = arr[i + 1];
-                }
-
-                arr[size - 1] = error;
-                size--;
-            }
-
-            else
-            {
-                Console.WriteLine("index is out of Range");
-            }
-        }
-
-    public T this[int index]
-        {
-            get { return arr[index]; }
-        }
-            
-
-      
-    }
     internal class Program
     {
+        
 
         static void Main(string[] args)
         {
-           Vector<int> vector = new Vector<int>();
-            vector.Add(1);
-            vector.Add(2);
-            vector.Add(3);
-            vector.Add(4);
-            vector.Add(5);
-            vector.Add(6);
-
-            vector.RemoveAt(4);
-            for (int i = 0; i < vector.Size(); i++)
-            {
-                Console.WriteLine(vector[i]);
-            }
+           
         }
     }
 }
